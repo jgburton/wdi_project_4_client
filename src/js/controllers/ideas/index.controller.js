0469d6fe025c3b91920ac1d4053d713aff9501f3
jgburton <jgranthamburton@gmail.com>
2017-02-21 13:22:42 +0000

    sorted out audio to play

--- src/js/controllers/ideas/index.controller.js
+++ src/js/controllers/ideas/index.controller.js
@@ -6,7 +6,11 @@ IdeasIndexCtrl.$inject = ['User', 'Idea', 'Addition'];
 function IdeasIndexCtrl(User, Idea, Addition){
   const vm = this;
 
-  vm.ideas = Idea.query();
+  Idea.query().$promise.then(data => {
+    vm.ideas = data;
+    console.log(vm.ideas);
+  });
+
   vm.$audio = $('audio');
 
   vm.playAll = () => {
@@ -17,9 +21,10 @@ function IdeasIndexCtrl(User, Idea, Addition){
     vm.$audio.each((tag, element) => element.pause());
   };
 
-  // TESTING API ROUTES FOR BETTER JSON OVERVIEW
-  Idea.query().$promise.then(data => console.log('Ideas', data));
-  Addition.query().$promise.then(data => console.log('Additions', data));
-  User.query().$promise.then(data => console.log('Users', data));
-  Idea.get({id: 1}).$promise.then(data => console.log('Idea 1', data));
+
+  // // TESTING API ROUTES FOR BETTER JSON OVERVIEW
+  // Idea.query().$promise.then(data => console.log('Ideas', data));
+  // Addition.query().$promise.then(data => console.log('Additions', data));
+  // User.query().$promise.then(data => console.log('Users', data));
+  // Idea.get({id: 1}).$promise.then(data => console.log('Idea 1', data));
 }
