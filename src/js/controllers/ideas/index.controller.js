d19ee6bf1866e2531984b1870bf55c663b7dc336
richardssinclair <richardssinclair@gmail.com>
2017-02-21 10:42:25 +0000

    back end and audio controls working

--- src/js/controllers/ideas/index.controller.js
+++ src/js/controllers/ideas/index.controller.js
@@ -2,10 +2,24 @@ angular
   .module('angularAuthentication')
   .controller('IdeasIndexCtrl', IdeasIndexCtrl);
 
-IdeasIndexCtrl.$inject = ['Idea'];
-function IdeasIndexCtrl(Idea){
+IdeasIndexCtrl.$inject = ['User', 'Idea', 'Addition'];
+function IdeasIndexCtrl(User, Idea, Addition){
   const vm = this;
 
   vm.ideas = Idea.query();
-  console.log(vm.ideas);
+  vm.$audio = $('audio');
+
+  vm.playAll = () => {
+    vm.$audio.each((tag, element) => element.play());
+  };
+
+  vm.pauseAll = () => {
+    vm.$audio.each((tag, element) => element.pause());
+  };
+
+  // TESTING API ROUTES FOR BETTER JSON OVERVIEW
+  Idea.query().$promise.then(data => console.log('Ideas', data));
+  Addition.query().$promise.then(data => console.log('Additions', data));
+  User.query().$promise.then(data => console.log('Users', data));
+  Idea.get({id: 1}).$promise.then(data => console.log('Idea 1', data));
 }
