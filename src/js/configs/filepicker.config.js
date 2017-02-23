f94cce229f12f68b60b5a91cc8c09e796d38d9f5
richardssinclair <richardssinclair@gmail.com>
2017-02-23 10:09:04 +0000

    adding styling to home page

--- src/js/configs/filepicker.config.js
+++ src/js/configs/filepicker.config.js
@@ -3,3 +3,8 @@ angular
 .config(function (filepickerProvider) {
   filepickerProvider.setKey('Au0FfUuSTvihNwvzZV0Q3z');
 });
+
+
+	// A7oUB4j1TYmHPZ1dWbCA9z
+
+  // Jamies key : Au0FfUuSTvihNwvzZV0Q3z
