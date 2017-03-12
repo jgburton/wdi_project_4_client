9dc96bf0580a6945bad897e3d4f0a2eb30291278
richardssinclair <richardssinclair@gmail.com>
2017-03-12 16:07:49 +0000

    changing link in api constant file for heroku

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,6 @@
 angular
   .module('remoteComposer')
-  .constant('API', `http://localhost:3000/api`);
+  .constant('API', `https://remotecomposerapi.herokuapp.com/api`);
+  //replace this with heroku link
+  //deploy fornt end and back end
+//take back end link from heroku and put it in here
